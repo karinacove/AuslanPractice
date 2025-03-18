8520d4f40a4e6022a20517f3057680d6afa5d523
karinacove <karina.cove@education.vic.gov.au>
2025-03-18 13:44:56 +1100

    Update script.js

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function updateGrid() {
     cells.forEach((cell, index) => {
         cell.textContent = currentGuess[index] || "";
         cell.style.fontFamily = "'AuslanFingerSpelling', sans-serif";
+        cell.style.fontSize = "48px";
     });
 }
 
