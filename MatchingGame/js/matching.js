7381ad66e2d63957d5eaa4f6f0b70b5d6f6d3d84
karinacove <karina.cove@education.vic.gov.au>
2025-06-20 10:04:38 +1000

    Update matching.js

--- MatchingGame/js/matching.js
+++ MatchingGame/js/matching.js
@@ -307,7 +307,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.appendChild(form);
     form.submit();
 
-    endModal.style.display = "flex";
+    Modal.style.display = "flex";
     
     document.getElementById("again-btn").addEventListener("click", () => {
       location.reload();
