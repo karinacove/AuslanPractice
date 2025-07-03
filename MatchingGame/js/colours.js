8b78e1f8005bb237e7a57a55dca336d927c12eb8
karinacove <karina.cove@education.vic.gov.au>
2025-07-03 19:37:10 +1000

    Update colours.js

--- MatchingGame/js/colours.js
+++ MatchingGame/js/colours.js
@@ -31,20 +31,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
  const formEntryIDs = {
   correct: [
-    "entry.1249394203", // Level 1 Correct
-    "entry.1551220511", // Level 2 Correct
-    "entry.903633326",  // Level 3 Correct
-    "entry.497882042",  // Level 4 Correct
-    "entry.1591755601", // Level 5 Correct
-    "entry.856597282"   // Level 6 Correct 
+    "entry.1897227570", // Level 1 Correct
+    "entry.1116300030", // Level 2 Correct
+    "entry.187975538",  // Level 3 Correct
+    "entry.1880514176", // Level 4 Correct
+    "entry.552536101",  // Level 5 Correct
+    "entry.922308538"   // Level 6 Correct  
   ],
   incorrect: [
-    "entry.1897227570", // Level 1 Incorrect
-    "entry.1116300030", // Level 2 Incorrect
-    "entry.187975538",  // Level 3 Incorrect
-    "entry.1880514176", // Level 4 Incorrect
-    "entry.552536101",  // Level 5 Incorrect
-    "entry.922308538"   // Level 6 Incorrect
+    "entry.1249394203", // Level 1 Inorrect
+    "entry.1551220511", // Level 2 Incorrect
+    "entry.903633326",  // Level 3 Incorrect
+    "entry.497882042",  // Level 4 Incorrect
+    "entry.1591755601", // Level 5 Incorrect
+    "entry.856597282"   // Level 6 Incorrect 
   ]
 };
 
